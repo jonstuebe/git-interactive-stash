c6f4c13c8b34936d250e156d223400ff66bc7b68
Jon Stuebe <jstuebe@gmail.com>
2021-11-13 15:16:28 -0700

    finished up confirms

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,48 +1,97 @@
 import React, { useEffect, useState } from "react";
-import simpleGit, {
-	DefaultLogFields,
-	ListLogLine,
-	LogResult,
-} from "simple-git";
-import { Box, Text } from "ink";
-import Spinner from "ink-spinner";
+import simpleGit, { DefaultLogFields, ListLogLine } from "simple-git";
+import { Text, TextProps, useApp, useInput } from "ink";
 import SelectInput from "ink-select-input";
 import { formatRelative, parseISO } from "date-fns";
 
 const git = simpleGit();
 
+function ConfirmInput({
+	message,
+	messageProps,
+	defaultConfirmed = true,
+	onSubmit,
+}: {
+	message: string;
+	messageProps?: TextProps;
+	defaultConfirmed: boolean;
+	onSubmit: (confirmed: boolean) => void;
+}) {
+	const [confirmed, setConfirmed] = useState(defaultConfirmed);
+	useInput((input, key) => {
+		if (input === "y") {
+			setConfirmed(true);
+		} else if (input === "n") {
+			setConfirmed(false);
+		}
+
+		if (key.return) {
+			onSubmit(confirmed);
+		}
+	});
+
+	return (
+		<Text {...messageProps}>
+			{message} ({defaultConfirmed === true ? "Y/n" : "y/N"})
+		</Text>
+	);
+}
+
 function App() {
+	const { exit } = useApp();
 	const [stashes, setStashes] = useState<
 		readonly (DefaultLogFields & ListLogLine)[]
 	>([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const [selectedStashIndex, setSelectedStashIndex] = useState<
+		string | undefined
+	>();
 
 	useEffect(() => {
 		(async () => {
 			const stashes = (await git.stashList()).all;
-			console.log(stashes);
 			setStashes(stashes);
-			setIsLoading(false);
 		})();
 	}, []);
 
-	if (isLoading) {
-		return <Spinner type="dots" />;
+	if (selectedStashIndex) {
+		return (
+			<>
+				<ConfirmInput
+					message="Remove the stash after applying it?"
+					messageProps={{
+						color: "cyan",
+					}}
+					defaultConfirmed
+					onSubmit={async (confirmed) => {
+						if (confirmed) {
+							await git.stash([
+								confirmed ? "pop" : "apply",
+								"--index",
+								selectedStashIndex.toString(),
+							]);
+						}
+
+						exit();
+					}}
+				/>
+			</>
+		);
 	}
 
+	// choose stash ui
 	return (
 		<>
-			<Text bold>Stashes:</Text>
+			<Text bold>Choose Stash</Text>
 			<SelectInput
-				items={stashes.map((stash) => ({
+				items={stashes.map((stash, idx) => ({
 					label: `${stash.message} (${formatRelative(
 						parseISO(stash.date),
 						new Date()
 					)})`,
-					value: stash.hash,
+					value: idx,
 				}))}
-				onSelect={async (item) => {
-					// await git.stash({ show })
+				onSelect={(item) => {
+					setSelectedStashIndex(item.value.toString());
 				}}
 			/>
 		</>
